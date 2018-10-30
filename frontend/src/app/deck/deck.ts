c1b066114f148bac09232173a1217310041dcd9b
ALEFF FRAZAO SANTANA (X04464162150) <x04464162150@tcu.gov.br>
2018-10-30 15:41:52 -0300

    Alterações entidade

--- frontend/src/app/deck/deck.ts
+++ frontend/src/app/deck/deck.ts
@@ -1,11 +1,14 @@
 export class Deck {
+    private _id: number;
     private _name: string;
+    private _types: Array<any>;
     private _raritys: Array<any>;
     private _cards: Array<any>;
 
-    constructor(name, types, cards) {
+    constructor(name, types, raritys, cards) {
         this._name = name;
-        this._raritys = types;
+        this._types = types;
+        this._raritys = raritys;
         this._cards = cards;
         return this;
     }
