4cd41eba384a09fffe327bfe320510bac5e2b116
ALEFF FRAZAO SANTANA (X04464162150) <x04464162150@tcu.gov.br>
2018-10-31 08:04:41 -0300

    tslint and ident

--- frontend/src/app/deck/deck.service.ts
+++ frontend/src/app/deck/deck.service.ts
@@ -10,32 +10,32 @@ import { Deck } from '../entity/deck';
 
 export class DeckService {
 
-  private url = Resource.getUrl() + "/deck";
+  private url = Resource.getUrl() + '/deck';
 
   constructor(private http: HttpClient) { }
 
 
-  setDeck(deck:Deck):Observable<any>{
+  setDeck(deck: Deck): Observable<any> {
     return this.http.post(this.url, deck);
   }
 
-  getDecks(): Observable<Deck[]>{
+  getDecks(): Observable<Deck[]> {
     return this.http.get<Deck[]>(this.url);
   }
-/*
-  getDecks() {
-    return [
-      {
-        'id': 1,
-        'name': 'nome do deck',
-        'types': ['blue', 'red', 'black', 'green', 'white'],
-        'raritys': {
-          'm': 10,
-          'r': 12,
-          'u': 14,
-          'c': 18
+  /*
+    getDecks() {
+      return [
+        {
+          'id': 1,
+          'name': 'nome do deck',
+          'types': ['blue', 'red', 'black', 'green', 'white'],
+          'raritys': {
+            'm': 10,
+            'r': 12,
+            'u': 14,
+            'c': 18
+          }
         }
-      }
-    ]
-  }*/
+      ]
+    }*/
 }
