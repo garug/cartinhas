2b9c5507926237f3d06c47620a5974b3f3fe5770
garug <allefff@gmail.com>
2018-10-30 22:00:36 -0300

    Initial ComponentsModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,22 @@
+import { ComponentsModule } from './components/components.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeckModule } from './deck/deck.module';
-import { CadastrarDeck } from './deck/cadastrar/cadastrar-deck.component';
 import { HomeComponent } from './home/home.component';
-import { DeckItemComponent } from './components/deck-item/deck-item.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { BarTypesComponent } from './components/bar-types/bar-types.component';
-import { DeckRoutingModule } from './deck/deck-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DeckItemComponent,
-    BarTypesComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    ComponentsModule,
     DeckModule,
     AppRoutingModule,
     HttpClientModule
--- frontend/src/app/deck/deck.module.ts
+++ frontend/src/app/deck/deck.module.ts
@@ -1,3 +1,4 @@
+import { ComponentsModule } from './../components/components.module';
 import { DeckRoutingModule } from './deck-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,11 +7,16 @@ import { DeckService } from './deck.service';
 
 import { CadastrarDeck } from './cadastrar/cadastrar-deck.component';
 import { ViewDeckComponent } from './view-deck/view-deck.component';
+import { BarTypesComponent } from '../components/bar-types/bar-types.component';
 
 @NgModule({
     declarations: [CadastrarDeck, ViewDeckComponent],
-    imports: [CommonModule, DeckRoutingModule],
-    exports: [CadastrarDeck],
+    imports: [
+        CommonModule,
+        ComponentsModule,
+        DeckRoutingModule
+    ],
+    exports: [],
     providers: [DeckService],
 })
 
