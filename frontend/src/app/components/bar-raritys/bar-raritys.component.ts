f9ba6b16b298444e9787aaf61c0a289a6c21542f
garug <allefff@gmail.com>
2018-11-01 19:55:46 -0300

    BarRaritys mostrando números de forma correta

--- frontend/src/app/components/bar-raritys/bar-raritys.component.ts
+++ frontend/src/app/components/bar-raritys/bar-raritys.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bar-raritys',
   templateUrl: './bar-raritys.component.html'
 })
 export class BarRaritysComponent implements OnInit {
+  @Input() deck = null;
 
   constructor() { }
 
