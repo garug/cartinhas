f1be88be12c194d5ad16cb8bea728a8cc2fa918b
garug <allefff@gmail.com>
2018-11-01 23:23:38 -0300

    Recebendo deck

--- frontend/src/app/deck/view-deck/view-deck.component.ts
+++ frontend/src/app/deck/view-deck/view-deck.component.ts
@@ -1,4 +1,7 @@
+import { Deck } from './../models/deck';
+import { DeckService } from './../deck.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-deck',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-deck.component.scss']
 })
 export class ViewDeckComponent implements OnInit {
+  deck: Deck = new Deck();
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private deckService: DeckService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(p => {
+      this.deckService.callDeck(p['id']).subscribe(d => this.deck = new Deck(d));
+    });
   }
 
 }
