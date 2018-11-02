13e9fdf210892077cc47ffc61f48256d978119f8
garug <allefff@gmail.com>
2018-11-01 23:25:07 -0300

    Recuperando deck por ID

--- backend/src/main/java/br/com/cartinhas/controller/DeckController.java
+++ backend/src/main/java/br/com/cartinhas/controller/DeckController.java
@@ -4,12 +4,14 @@ import br.com.cartinhas.entity.Deck;
 import br.com.cartinhas.repository.DeckRepository;
 import br.com.cartinhas.service.DeckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("deck")
@@ -20,6 +22,11 @@ public class DeckController {
 
     @Autowired
     private DeckService deckService;
+    
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public Optional<Deck> getById(@PathVariable("id") Long id) {
+    	return deckRepository.findById(id);
+    }
 
     @RequestMapping(value="", method = RequestMethod.GET)
     public List<Deck> getDeck(){
