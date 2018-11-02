213737a68ebcc0b3dab7a53ddc9a9f1432105907
garug <allefff@gmail.com>
2018-11-02 07:36:02 -0300

    Diminuindo verbosidade de controlador rest

--- backend/src/main/java/br/com/cartinhas/controller/DeckController.java
+++ backend/src/main/java/br/com/cartinhas/controller/DeckController.java
@@ -4,7 +4,10 @@ import br.com.cartinhas.entity.Deck;
 import br.com.cartinhas.repository.DeckRepository;
 import br.com.cartinhas.service.DeckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,22 +26,22 @@ public class DeckController {
     @Autowired
     private DeckService deckService;
     
-    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    @GetMapping
     public Optional<Deck> getById(@PathVariable("id") Long id) {
     	return deckRepository.findById(id);
     }
 
-    @RequestMapping(value="", method = RequestMethod.GET)
+    @GetMapping
     public List<Deck> getDeck(){
         return deckRepository.findAll();
     }
 
-    @RequestMapping(value="", method = RequestMethod.POST)
+    @PostMapping
     public Deck setDeck(@RequestBody Deck deck){
         return deckService.save(deck);
     }
 
-    @RequestMapping(value="", method = RequestMethod.PUT)
+    @PutMapping
     public Deck batata(@RequestBody Deck deck){
         return deckRepository.save(deck);
     }
