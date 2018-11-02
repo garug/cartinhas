478e074de120c1c21a8622bd4bbc117c74fb4084
garug <allefff@gmail.com>
2018-11-02 13:11:07 -0300

    Carga inicial e ligação com api

--- backend/src/main/java/br/com/cartinhas/config/CargaInicial.java
+++ backend/src/main/java/br/com/cartinhas/config/CargaInicial.java
@@ -1,17 +1,21 @@
 package br.com.cartinhas.config;
 
-import br.com.cartinhas.entity.Color;
-import br.com.cartinhas.entity.Deck;
-import br.com.cartinhas.enuns.EColor;
-import br.com.cartinhas.repository.ColorRepository;
-import br.com.cartinhas.repository.DeckRepository;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
+import br.com.cartinhas.entity.Card;
+import br.com.cartinhas.entity.Color;
+import br.com.cartinhas.entity.Deck;
+import br.com.cartinhas.enuns.EColor;
+import br.com.cartinhas.repository.CardRepository;
+import br.com.cartinhas.repository.ColorRepository;
+import br.com.cartinhas.repository.DeckRepository;
+import io.magicthegathering.javasdk.api.CardAPI;
 
 @Component
 public class CargaInicial implements ApplicationListener<ContextRefreshedEvent> {
@@ -21,16 +25,28 @@ public class CargaInicial implements ApplicationListener<ContextRefreshedEvent> 
 
     @Autowired
     private ColorRepository colorRepository;
+    
+    @Autowired
+    private CardRepository cardRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
-        Color color = new Color(EColor.AZUL);
+        Color color = new Color(EColor.Blue);
         colorRepository.save(color);
-        color = new Color(EColor.PRETO);
+        color = new Color(EColor.Black);
         colorRepository.save(color);
 
         List<Deck> decks = deckRepository.findAll();
+        
+        List<String> filter = new ArrayList<String>();
+        filter.add("name=teferi");
+        
+        List<io.magicthegathering.javasdk.resource.Card> cardsAPI = CardAPI.getAllCards(filter);
+        
+        for(io.magicthegathering.javasdk.resource.Card cardAPI : cardsAPI){
+        	cardRepository.save(new Card(cardAPI.getId(), cardAPI.getColors(), cardAPI.getManaCost(), cardAPI.getRarity(), cardAPI.getTypes()));
+        }
 
         if (decks.isEmpty()) {
 
