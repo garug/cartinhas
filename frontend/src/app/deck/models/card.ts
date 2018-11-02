1d63377704b41a451678b4d6a4d222d4853dec92
garug <allefff@gmail.com>
2018-11-02 18:28:43 -0300

    Melhorias em modelagem

--- frontend/src/app/deck/models/card.ts
+++ frontend/src/app/deck/models/card.ts
@@ -2,9 +2,14 @@ export class Card {
     private _id: Number;
     private _name: String;
     private _rarity: String;
+    private _types: any;
     private _colors: Array<any>;
 
     set name(name: String) {
         this._name = name;
+    }
+
+    get types() {
+        return this._types;
     }
 }
