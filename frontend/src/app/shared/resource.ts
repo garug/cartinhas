3d9054346f9269dff6c2b59f8e6bd2a621ffc9ee
garug <allefff@gmail.com>
2018-11-02 15:31:21 -0300

    Inserção api

--- frontend/src/app/shared/resource.ts
+++ frontend/src/app/shared/resource.ts
@@ -3,4 +3,8 @@ export class Resource {
     public static getUrl() {
         return 'http://localhost:8080';
     }
+
+    public static apiUrl() {
+        return 'https://api.magicthegathering.io/v1/cards';
+    }
 }
