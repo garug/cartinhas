aeb750b496a03dcc4115f7ec80ab83ab8577790e
garug <allefff@gmail.com>
2018-11-21 15:44:49 -0200

    ImageUrl added to card model

--- frontend/src/app/deck/models/card.ts
+++ frontend/src/app/deck/models/card.ts
@@ -4,6 +4,7 @@ export class Card {
     private _rarity: String;
     private _types: any;
     private _colors: Array<any>;
+    private _imageUrl: string;
 
     set name(name: String) {
         this._name = name;
@@ -15,5 +16,9 @@ export class Card {
 
     get types() {
         return this._types;
+    }
+
+    get imageUrl() {
+        return this._imageUrl;
     }
 }
