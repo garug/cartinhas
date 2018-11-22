9cca2ee3adf27c29a23bf2d8f44ea999d33da736
garug <allefff@gmail.com>
2018-11-22 15:21:47 -0200

    Animação de entrada de decks

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { DeckService } from '../deck/deck.service';
+import { trigger, transition, style, animate, state, query, stagger, keyframes } from '@angular/animations';
 
+import { DeckService } from '../deck/deck.service';
 import { Deck } from './../deck/models/deck';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('animated', [
+      transition('* => *', [
+        query(':enter', [
+          style({ transform: 'translateY(-20%)', opacity: 0 }),
+          stagger(150, [
+            animate('0.25s', keyframes([
+              style({transform: 'translateY(-15%)',  opacity: .2 }),
+              style({transform: 'translateY(-10%)',  opacity: .5 }),
+              style({transform: 'translateY(0)',  opacity: 1 })
+            ]))
+          ])
+        ], {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -19,6 +36,10 @@ export class HomeComponent implements OnInit {
   }
 
   getDecks() {
-     this.deckService.getDecks().subscribe(response =>  this.listDecks = response);
+    this.deckService.getDecks().subscribe(response => this.listDecks = response);
+  }
+
+  teste(i) {
+    console.log(i);
   }
 }
